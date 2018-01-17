85418874e89db973a30e769ab0c2bb2dc69b48cf
mengziQ <moekoasano0829@gmail.com>
2018-01-17 06:46:07 +0000

    一旦作業中止。

--- gradient-boosting/lightgbm/classification.py
+++ gradient-boosting/lightgbm/classification.py
@@ -3,7 +3,31 @@
 # scikit-learnをインストールする前に、scipyがあるか確認した方がいい
 
 from sklearn.datasets import load_iris
+# cross_validationはそのうち廃止されるので使わない
+from sklearn.model_selection import train_test_split
+import lightgbm as lgb
 
 iris = load_iris()
-xs = iris.data
-ys = iris.target
+X = iris.data
+y = iris.target
+
+# random_stateとは、乱数の初期値。指定することで、毎回同じ乱数が生成される。つまり何回試行しても同じデータが学習・テストに使用される。
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size=0.2, random_state=0)
+
+lgb_train =  lgb.Dataset(X_train, y_train)
+lgb_evel = lgb.Dataset(X_test, y_test, reference=lgb_train)
+
+# LightGBM parameters
+params = {
+  'task':'train'
+  'boosting_type':'gbdt'
+  'objective':'multiclass'
+  'metric':
+  'num_class':
+  'learning_rate':
+  'num_leaves':
+  'min_data_in_leaf':
+  'num_iteration':
+  'vervose':
+}
