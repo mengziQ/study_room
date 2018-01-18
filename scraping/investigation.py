5ee8a026499aa053f40952ff726051478a9378f9
mengziQ <moekoasano0829@gmail.com>
2018-01-18 09:30:59 +0000

    update

--- scraping/investigation.py
+++ scraping/investigation.py
@@ -6,22 +6,14 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20
 u = 'http://catindog.hatenablog.com/archive/2017/'
 links = []
 
-for page in range(11):
+for page in range(12):
   url = u + str(page + 1)
-  #print(url)
+  print(url)
   resp = requests.get(url, timeout=1, headers=headers)
   soup = BeautifulSoup(resp.text, 'html.parser')
   a_tag = soup.find_all('a', class_='entry-title-link')
-  #print(a_tag)
-  #if len(a_tag) > 1:
+
   for a in a_tag:
     print(a['href'])
-  #else:
-   # print(a_tag['href'])
 
-#links.append(link)
-
-#with open('link.txt', 'w') as l:
- # for li in links:
-  #  l.write(li)
 
