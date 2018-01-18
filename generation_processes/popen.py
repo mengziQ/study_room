ec786d3c5fe8264483c88603587bc840e307893a
mengziQ <moekoasano0829@gmail.com>
2018-01-18 10:29:42 +0000

    subprocess難解

--- generation_processes/popen.py
+++ generation_processes/popen.py
@@ -1,5 +1,12 @@
-import os, sys
+import os
+import subprocess
+from subprocess import Popen 
 
-cmd = 'mkdir nwdir'
-fo = os.popen(cmd, 'r', 1)
+cmd1 = 'cd nwdir| wget "https://safebooru.org/index.php?page=post&s=view&id=2429568"'
+# os.popenの方はcmd1成功、subprocess.call, Popenだとエラー
+#fo = os.popen(cmd1)
+fo = Popen(cmd1)
 print(fo)
+
+# エラー
+#proc = subprocess.call(cmd.strip().split(" "))
