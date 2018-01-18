4cad48db45e714ef052d9357080ffd03e6e6ed56
mengziQ <moekoasano0829@gmail.com>
2018-01-18 10:03:09 +0000

    各記事をDLするところまで。結局subprocessの使い方が分からなかった・・・

--- scraping/investigation.py
+++ scraping/investigation.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import wget
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"}
 
+# 日付CSVとかにする？
 u = 'http://catindog.hatenablog.com/archive/2017/'
 links = []
 
@@ -14,6 +17,11 @@ for page in range(12):
   a_tag = soup.find_all('a', class_='entry-title-link')
 
   for a in a_tag:
-    print(a['href'])
+    links.append(a['href'])
+
+for li in links:
+  os.chdir('/home/irep/repos/study_room/scraping/page_data')
+  wget.download(li)
+
 
 
