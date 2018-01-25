20b6d6702127c300d33bfa9da4f2f46a66a8e219
mengziQ <moekoasano0829@gmail.com>
2018-01-25 04:59:11 +0000

    舐められてる

---
+++ deep-learning/deep-learning-from-zero/2-3-3.py
@@ -0,0 +1,46 @@
+import numpy as np
+import sys
+
+# 同じ処理何回も書いてるけどごめんなさい、直すの面倒
+
+def AND(x):
+  w = np.array([0.5, 0.5])
+  b = -0.7
+  tmp = np.sum((x * w)) + b
+
+  if tmp <= 0:
+    return 0
+  elif tmp > 0:
+    return 1
+
+def NAND(x):
+  w = np.array([-0.5, -0.5])
+  b = 0.7
+  tmp = np.sum((x * w)) + b
+
+  if tmp <= 0:
+    return 0
+  elif tmp > 0:
+    return 1 
+
+def OR(x):
+  w = np.array([0.5, 0.5])
+  b = -0.2
+  tmp = np.sum((x * w)) + b
+
+  if tmp <= 0:
+    return 0
+  elif tmp > 0:
+    return 1 
+
+args = sys.argv
+x1, x2 = map(int, input().split(' ')) 
+x = np.array([x1, x2])
+
+
+if args[1] == 'and': 
+  print(AND(x))                                                                                                                                                                                     
+elif args[1] == 'nand':
+  print(NAND(x))
+elif args[1] == 'or':
+  print(OR(x))
