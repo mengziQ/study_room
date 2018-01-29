3a737bdf96456126259fb54a5cf836b81491657e
mengziQ <moekoasano0829@gmail.com>
2018-01-29 08:37:08 +0000

    ビルず

---
+++ deep-learning/deep-learning-from-zero/3-4-2.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# sigmoid関数は活性化関数のコードからコピペ
+def sigmoid(x_array):
+  y = []
+  for x in x_array:
+    y.append(1/(1 + np.exp(-x)))
+  return y
+
+def indentity_function(x):
+  return x
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5],[0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid(A1)
+
+#print(A1)
+#print(Z1)
+
+W2 = np.array([[0.1, 0.4],[0.2, 0.5],[0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+W3 = np.array([[0.1, 0.3],[0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3 ) + B3
+y = indentity_function(A3)
+
+print(y)
+
