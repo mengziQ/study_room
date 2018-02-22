f08b4a2452d474a0c0206041c323d4347f9b53e6
mengziQ <moekoasano0829@gmail.com>
2018-02-22 04:28:21 +0000

    コピペでもip取得できないので環境の問題かもしれない

--- google_cloud_function/railgun/check_railgun.py
+++ google_cloud_function/railgun/check_railgun.py
@@ -5,13 +5,13 @@ import json
 ipaddrs = [] 
 
 def _map1(arr):
-  ipaddr = os.popen('curl -s https://us-central1-machine-learning-173502.cloudfunctions.net/shotgun').read()
+  ipaddr = os.popen('curl -s https://us-central1-machine-learning-173502.cloudfunctions.net/railgun').read()
   print('ipaddr', arr, ipaddr.strip())
   ipaddrs.append(ipaddr.strip())
   return ipaddr 
 
 arrs = [i for i in range(1024)]
-with concurrent.futures.ThreadPoolExecutor(max_workers=1024) as exe:
+with concurrent.futures.ThreadPoolExecutor(max_workers=200) as exe:
   exe.map(_map1, arrs)
 
 open('ipaddrs.json', 'w').write( json.dumps(ipaddrs, indent=2) )
